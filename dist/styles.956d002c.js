9856ad1cceb5725a2862a48daee964c400d1409f
denis m <denis.markin.95@gmail.com>
2023-03-06 16:39:00 +0400

    banner-section-complited

--- dist/styles.956d002c.js
+++ dist/styles.956d002c.js
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56266" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58946" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
