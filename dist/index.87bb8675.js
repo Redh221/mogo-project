a48e390617806790da30950eec0c74425e94dd2f
denis m <denis.markin.95@gmail.com>
2023-03-14 21:25:39 +0400

    parcel2

---
+++ dist/index.87bb8675.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".header__sidebar"),t=document.querySelector(".header__burger");document.addEventListener("DOMContentLoaded",(()=>{t.addEventListener("click",(()=>e.classList.toggle("header__sidebar_active")))}));
+//# sourceMappingURL=index.87bb8675.js.map
